42ad2933009be2148790b8bf1f5e2619b7797fe9
DefectingCat <i@defect.ink>
2023-03-13 10:18:52 +0800

    impl executor run

--- src/executor.rs
+++ src/executor.rs
@@ -1,37 +1,31 @@
 use std::pin::Pin;
 
-use crate::waker::TimerFuture;
-
 use {
-    futures::{
-        future::FutureExt,
-        task::{waker_ref, ArcWake},
-    },
+    futures::task::{waker_ref, ArcWake},
     std::{
         future::Future,
         sync::mpsc::{sync_channel, Receiver, SyncSender},
         sync::{Arc, Mutex},
-        task::{Context, Poll},
-        time::Duration,
+        task::Context,
     },
     // 引入之前实现的定时器模块
 };
 
 pub struct Executor {
-    ready_queue: Receiver<Arc<Task>>,
+    pub ready_queue: Receiver<Arc<Task>>,
 }
 
 #[derive(Clone)]
 pub struct Spawner {
-    task_sender: SyncSender<Arc<Task>>,
+    pub task_sender: SyncSender<Arc<Task>>,
 }
 
 type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'static>>;
 
-struct Task {
-    future: Mutex<Option<BoxFuture<'static, ()>>>,
+pub struct Task {
+    pub future: Mutex<Option<BoxFuture<'static, ()>>>,
 
-    task_sender: SyncSender<Arc<Task>>,
+    pub task_sender: SyncSender<Arc<Task>>,
 }
 
 pub fn new_executor_and_spawner() -> (Executor, Spawner) {
@@ -48,5 +42,27 @@ impl Spawner {
             task_sender: self.task_sender.clone(),
         });
         self.task_sender.send(task).expect("任务队列已满");
+    }
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let cloned = arc_self.clone();
+        arc_self.task_sender.send(cloned).expect("任务队列已满");
+    }
+}
+
+impl Executor {
+    pub fn run(&self) {
+        while let Ok(task) = self.ready_queue.recv() {
+            let mut future_slot = task.future.lock().unwrap();
+            if let Some(mut future) = future_slot.take() {
+                let waker = waker_ref(&task);
+                let context = &mut Context::from_waker(&*waker);
+                if future.as_mut().poll(context).is_pending() {
+                    *future_slot = Some(future)
+                }
+            }
+        }
     }
 }
