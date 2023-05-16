da148d4e591837f6a62a71ae8e6bdcf921dfa8e9
DefectingCat <i@defect.ink>
2023-05-16 17:53:34 +0800

    update comment

--- src/executor.rs
+++ src/executor.rs
@@ -10,6 +10,7 @@ use {
     },
 };
 
+/// 任务执行器，负责从通道中接收任务然后执行
 pub struct Executor {
     pub ready_queue: Receiver<Arc<Task>>,
 }
@@ -21,18 +22,30 @@ pub struct Spawner {
 
 type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'static>>;
 
+/// 一个Future，它可以调度自己(将自己放入任务通道中)，然后等待执行器去`poll`
 pub struct Task {
+    /// 进行中的Future，在未来的某个时间点会被完成
+    ///
+    /// 按理来说`Mutex`在这里是多余的，因为我们只有一个线程来执行任务。但是由于
+    /// Rust并不聪明，它无法知道`Future`只会在一个线程内被修改，并不会被跨线程修改。因此
+    /// 我们需要使用`Mutex`来满足这个笨笨的编译器对线程安全的执着。
+    ///
+    /// 如果是生产级的执行器实现，不会使用`Mutex`，因为会带来性能上的开销，取而代之的是使用`UnsafeCell`
     pub future: Mutex<Option<BoxFuture<'static, ()>>>,
 
+    /// 可以将该任务自身放回到任务通道中，等待执行器的poll
     pub task_sender: SyncSender<Arc<Task>>,
 }
 
 pub fn new_executor_and_spawner() -> (Executor, Spawner) {
+    // 任务通道允许的最大缓冲数(任务队列的最大长度)
+    // 当前的实现仅仅是为了简单，在实际的执行中，并不会这么使用
     const MAX_QUEUED_TAKSS: usize = 10_000;
     let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TAKSS);
     (Executor { ready_queue }, Spawner { task_sender })
 }
 
+/// `Spawner`负责创建新的`Future`然后将它发送到任务通道中
 impl Spawner {
     pub fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
         let future = Box::pin(future);
